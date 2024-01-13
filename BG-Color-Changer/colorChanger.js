7a3a9d231d347eb5ff3af9e3d04d8d24a8435264
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-13 00:12:14 -0600

    Add pink btn click event

--- BG-Color-Changer/colorChanger.js
+++ BG-Color-Changer/colorChanger.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Variable declarations
     let background = document.getElementById("colorContainer")
     let grayBtn = document.getElementById("grayBtn");
+    let pinkBtn = document.getElementById("pinkBtn");
 
+    // Gray button click event
     grayBtn.addEventListener("click", () => {
         background.style.backgroundColor = "#CCDAD1";
+    })
+
+    // Pink button click event
+    pinkBtn.addEventListener("click", () => {
+        background.style.backgroundColor = "#FF88DC";
     })
 })
