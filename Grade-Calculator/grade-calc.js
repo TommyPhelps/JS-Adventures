9a5b89e0e56dfd536fb307013583b331106d1952
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-15 22:15:13 -0600

    Change JS validation conditional statement to use the form to validate.

--- Grade-Calculator/grade-calc.js
+++ Grade-Calculator/grade-calc.js
@@ -1,9 +1,9 @@
 function calcGrade() {
-    if (document.correctQuestions.value === "") {
+    if (document.assignmentGrade.correctQuestions.value === "") {
         alert("Please enter the amount of correct questions");
         document.correctQuestions.focus();
     }
-    if (document.totalQuestions.value === "") {
+    if (document.assignmentGrade.totalQuestions.value === "") {
         alert("Please enter the amount of total questions");
         document.totalQuestions.focus();
     }
