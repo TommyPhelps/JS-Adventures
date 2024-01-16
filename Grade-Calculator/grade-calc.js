957636bb1b15ab75abd61c18bb528eda496fc000
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-15 22:21:12 -0600

    Alter JS conditional statements for validation to return false if empty

--- Grade-Calculator/grade-calc.js
+++ Grade-Calculator/grade-calc.js
@@ -2,9 +2,11 @@ function calcGrade() {
     if (document.assignmentGrade.correctQuestions.value === "") {
         alert("Please enter the amount of correct questions");
         document.correctQuestions.focus();
+        return false;
     }
     if (document.assignmentGrade.totalQuestions.value === "") {
         alert("Please enter the amount of total questions");
         document.totalQuestions.focus();
+        return false;
     }
 }
