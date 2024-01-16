302b2f843080187843dfb5e36b4b17b3e53d548f
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-16 06:44:05 -0600

    Change total questions variable name. Add integer validation.
    Changed the total questions variable from "assignmentGrade" to
    "totalQuestions". Added integer validation for text boxes through the
    use of coditional statements.

--- Grade-Calculator/grade-calc.js
+++ Grade-Calculator/grade-calc.js
@@ -1,15 +1,25 @@
 function calcGrade() {
     let correctQuestions = document.assignmentGrade.correctQuestions
-    let assignmentGrade = document.assignmentGrade.totalQuestions
+    let totalQuestions = document.totalQuestions.totalQuestions
 
     if (correctQuestions.value === "") {
         alert("Please enter the amount of correct questions");
-        document.assignmentGrade.correctQuestions.focus();
+        correctQuestions.focus();
         return false;
     }
     if (assignmentGrade.value === "") {
         alert("Please enter the amount of total questions");
-        document.assignmentGrade.totalQuestions.focus();
+        totalQuestions.focus();
+        return false;
+    }
+    if (isNaN(correctQuestions.value)) {
+        alert("Please enter a numeric value into the correct questions text box.");
+        correctQuestions.focus();
+        return false;
+    }
+    if (isNaN(totalQuestions.value)) {
+        alert("Please enter a numeric value into the correct questions text box.");
+        totalQuestions.focus();
         return false;
     }
 }
