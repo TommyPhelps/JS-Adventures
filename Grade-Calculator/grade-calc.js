bbf1a84bd2bc202cdad3d328ff2b23c7a5d44259
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-16 06:30:45 -0600

    Add variables to JS file to improve readability

--- Grade-Calculator/grade-calc.js
+++ Grade-Calculator/grade-calc.js
@@ -1,4 +1,7 @@
 function calcGrade() {
+    let correctQuestions = document.assignmentGrade.correctQuestions
+    let assignmentGrade = document.assignmentGrade.totalQuestions
+
     if (document.assignmentGrade.correctQuestions.value === "") {
         alert("Please enter the amount of correct questions");
         document.assignmentGrade.correctQuestions.focus();
