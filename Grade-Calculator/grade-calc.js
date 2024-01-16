90e4125a3b3e2bd9b2ff3fde92bafd0fd28d6eac
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-15 22:10:00 -0600

    Change HTML to use a form instead. Change equality operator for JS file.
    Changed HTML to use forms instead for validation. Changed equality
    opeartor from "==" to "===" for the validation in the JavaScript file.

--- Grade-Calculator/grade-calc.js
+++ Grade-Calculator/grade-calc.js
@@ -1,9 +1,9 @@
 function calcGrade() {
-    if (document.correctQuestions.value == "") {
+    if (document.correctQuestions.value === "") {
         alert("Please enter the amount of correct questions");
         document.correctQuestions.focus();
     }
-    if (document.totalQuestions.value == "") {
+    if (document.totalQuestions.value === "") {
         alert("Please enter the amount of total questions");
         document.totalQuestions.focus();
     }
