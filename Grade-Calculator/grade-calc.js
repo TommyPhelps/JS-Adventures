4cd12b48ff029165ec952d0d94ea2f1060c23838
Tommy Phelps <tommydustinphelps@gmail.com>
2024-01-16 06:36:16 -0600

    Add variables to conditional statements

--- Grade-Calculator/grade-calc.js
+++ Grade-Calculator/grade-calc.js
@@ -2,12 +2,12 @@ function calcGrade() {
     let correctQuestions = document.assignmentGrade.correctQuestions
     let assignmentGrade = document.assignmentGrade.totalQuestions
 
-    if (document.assignmentGrade.correctQuestions.value === "") {
+    if (correctQuestions.value === "") {
         alert("Please enter the amount of correct questions");
         document.assignmentGrade.correctQuestions.focus();
         return false;
     }
-    if (document.assignmentGrade.totalQuestions.value === "") {
+    if (assignmentGrade.value === "") {
         alert("Please enter the amount of total questions");
         document.assignmentGrade.totalQuestions.focus();
         return false;
